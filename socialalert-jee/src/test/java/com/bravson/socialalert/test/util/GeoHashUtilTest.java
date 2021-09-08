d6e24044e26db02fb0603cd44a04b2dcb6537ded
lucien <lgarin@gmx.ch>
2021-09-08 22:47:27 +0200

    removed dependency in GeoHashUtilTest

--- socialalert-jee/src/test/java/com/bravson/socialalert/test/util/GeoHashUtilTest.java
+++ socialalert-jee/src/test/java/com/bravson/socialalert/test/util/GeoHashUtilTest.java
@@ -4,25 +4,25 @@ import org.assertj.core.api.Assertions;
 import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
-import com.bravson.socialalert.domain.location.GeoBox;
 import com.bravson.socialalert.infrastructure.util.GeoHashUtil;
+
+import ch.hsr.geohash.BoundingBox;
 
 public class GeoHashUtilTest extends Assertions {
 	
 	 @Test
 	 public void computeBoundingBox() {
-		 GeoBox geoBox = GeoBox.fromBoundingBox(GeoHashUtil.computeBoundingBox("u0m9"));
-		 assertThat(geoBox.getMaxLat()).isEqualTo(46.75, Offset.offset(0.01));
-		 assertThat(geoBox.getMinLat()).isEqualTo(46.58, Offset.offset(0.01));
-		 assertThat(geoBox.getMaxLon()).isEqualTo(8.08, Offset.offset(0.01));
-		 assertThat(geoBox.getMinLon()).isEqualTo(7.73, Offset.offset(0.01));
+		 BoundingBox box = GeoHashUtil.computeBoundingBox("u0m9");
+		 assertThat(box.getMaxLat()).isEqualTo(46.75, Offset.offset(0.01));
+		 assertThat(box.getMinLat()).isEqualTo(46.58, Offset.offset(0.01));
+		 assertThat(box.getMaxLon()).isEqualTo(8.08, Offset.offset(0.01));
+		 assertThat(box.getMinLon()).isEqualTo(7.73, Offset.offset(0.01));
 	 }
 
 	 @Test
 	 public void computeGeoHashPrecision() {
-		 GeoBox geoBox = GeoBox.builder().maxLat(46.89713427117885).minLat(46.123042072708216)
-				 .maxLon(8.526546992361546).minLon(7.396524623036385).build();
-		 int precision = GeoHashUtil.computeGeoHashPrecision(geoBox.toBoundingBox(), 128);
+		 BoundingBox box = new BoundingBox(46.123042072708216, 46.89713427117885, 7.396524623036385, 8.526546992361546);
+		 int precision = GeoHashUtil.computeGeoHashPrecision(box, 128);
 		 assertThat(precision).isEqualTo(7);
 	 }
 }
