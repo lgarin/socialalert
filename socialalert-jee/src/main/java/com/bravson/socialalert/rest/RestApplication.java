db3095b89f497eb49d78d000ff4f95d18c2b5acc
lucien <lgarin@gmx.ch>
2021-12-04 11:14:32 +0100

    use property file for OpenAPI configuration

--- socialalert-jee/src/main/java/com/bravson/socialalert/rest/RestApplication.java
+++ socialalert-jee/src/main/java/com/bravson/socialalert/rest/RestApplication.java
@@ -3,19 +3,6 @@ package com.bravson.socialalert.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import org.eclipse.microprofile.openapi.annotations.Components;
-import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
-import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
-import org.eclipse.microprofile.openapi.annotations.info.Info;
-import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
-import org.eclipse.microprofile.openapi.annotations.servers.Server;
-
-@OpenAPIDefinition(
-		info = @Info(title = "Socialalert API", version = "2.21"),
-        servers = {@Server(description = "Test server", url = "http://3ft8uk98qmfq79pc.myfritz.net:18774"),
-        		   @Server(description = "Dev server", url = "http://localhost:7080")},
-        components = @Components(securitySchemes = @SecurityScheme(securitySchemeName = "JWT", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT"))
-)
 @ApplicationPath("/rest")
 public class RestApplication extends Application {
 
